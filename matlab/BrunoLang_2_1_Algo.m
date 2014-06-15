7ab82cb0385f9e1de9643ab2ea4637b40351368e
grantathon <grant.bartel@gmail.com>
2014-06-15 16:50:56 +0200

    Implemented algo, but issue with finding b (tridiags A twice)

--- matlab/BrunoLang_2_1_Algo.m
+++ matlab/BrunoLang_2_1_Algo.m
@@ -2,11 +2,13 @@ clear all;
 tic;
 
 % Inputs
-n = 7;
-d = 2;
-beta = 0;
-v = 1;
-% 
+n = 7;  % A_nxn (i.e., square A with dim n)
+d = 2;  % semi-diagonal size (e.g., 1 = trididagonal A)
+
+% Initialize unit vector
+unit = zeros(d, 1);
+unit(1) = 1;
+
 % Build symmetric Toeplitz
 % temp = rand(1, (diagRadius+1));
 % for i=(diagRadius + 2):dim
@@ -35,65 +37,72 @@ A = [   0.78 0.39 0.84 0.00 0.00 0.00 0.00;
         0.00 0.00 0.00 0.84 0.39 0.78 0.39;
         0.00 0.00 0.00 0.00 0.84 0.39 0.78; ]
 
-
-% Compute Householder parameters for first Q
-alpha = -sign(A(2, 1))*norm(A(2:n, 1));
-r = sqrt((alpha^2 - A(2, 1)*alpha) / 2);
-
-% Compute householder vector
-v = zeros(1, d+1);
-v(2) = (A(2, 1) - alpha) / (2*r);
-if((d+1) > 2)
-    for k=3:(d+1)
-        v(k) = A(k, 1) / (2*r);
+% Main loop
+for u = 1:(n-2)
+    % Determine b & r iteration parameters
+    [b, r] = BrunoLang_2_1_Find_b_r(n, d, u);
+    
+    % TODO: Find out what we should do when b & r are no longer computable
+    if(b == 0)
+        disp('The parameters b & r are undeterminable. Algo terminated!')
+        break;
     end
-end     
 
-% Compute Q1
-Q1 = eye(d+1) - 2*transpose(v)*v;
+    % Compute Householder parameters
+    alpha = -sign(A((u+1), u))*norm(A((u+1):(u+d), u));
+    rad = sqrt((alpha^2 - A((u+1), u)*alpha) / 2);
 
-% Compute new column of A
-A(2:d+1, 1) = Q1(2:d+1, 2:d+1)*A(2:d+1, 1);
-A(1, 2:d+1) = A(2:d+1, 1);
-
-% Compute the remaining block-diagonal
-A(2:d+1, 2:d+1) = Q1(2:d+1, 2:d+1)*A(2:d+1, 2:d+1)*Q1(2:d+1, 2:d+1);
-
-% Compute the block-diagonal's bottom-right neighbor blocks
-
-
-A
-
-beta = beta + 1;
-
-% Compute Householder parameters for next Q
-alpha = -sign(A(2+beta, 1+beta))*norm(A(2+beta:n, 1+beta));
-r = sqrt((alpha^2 - A(2+beta, 1+beta)*alpha) / 2);
-
-% Compute householder vector
-% v = zeros(1, dim-1);
-% v(2) = (A(2+1, 1+1) - alpha) / (2*r);
-% if((dim-1) >= 3)
-%     for k=3:(dim-1)
-%         v(k) = A(k, 1) / (2*r);
-%     end
-% end
-v = zeros(1, d+1);
-v(2) = (A(2+beta, 1+beta) - alpha) / (2*r);
-if((d+1) > 2)
+    % Compute Householder vector
+    v = zeros(d+1, 1);
+    v(2) = (A((u+1), u) - alpha) / (2*rad);
     for k=3:(d+1)
-        v(k) = A(k, 1) / (2*r);
+        v(k) = A((k + (u-1)), u) / (2*rad);
+    end
+    
+%     v = zeros(d, 1);
+%     v(1) = (A((u+1), u) - alpha) / (2*rad);
+%     for k=2:d
+%         v(k) = A(k, u) / (2*rad);
+%     end
+    
+    % Compute Q1
+    Q1 = eye(d) - 2*v(2:(d+1))*transpose(v(2:(d+1)));
+%     Q1 = eye(d) - 2*v*transpose(v);
+
+    % Compute new column/row of A
+    A((u+1):(u+d), u) = Q1 * A((u+1):(u+d), u);
+    A(u, (u+1):(u+d)) = A((u+1):(u+d), u);
+    
+    % Distribute the Qs among the other parts of A for proper trasformation
+    for beta = 1:b
+        bd_idx_a = u + 2*beta - 1;
+        bd_idx_b = u + beta*d;
+        
+        % Compute the block-diagonal
+        A(bd_idx_a:bd_idx_b, bd_idx_a:bd_idx_b) = transpose(Q1) * A(bd_idx_a:bd_idx_b, bd_idx_a:bd_idx_b) * Q1;
+        
+        % Compute the block-diagonal's bottom-right neighbor blocks
+        if(beta < b)
+            bd_idx_c = bd_idx_a + d;
+            bd_idx_d = bd_idx_b + d;
+            
+            % Compute Q2 for upper-/lower-triangularization of neighbors
+            A(bd_idx_c:bd_idx_d, bd_idx_a:bd_idx_b) = A(bd_idx_c:bd_idx_d, bd_idx_a:bd_idx_b) * Q1;
+            w = A(bd_idx_c:(bd_idx_d), bd_idx_a) + sign(A(bd_idx_c, bd_idx_a))*norm(A(bd_idx_c:(bd_idx_d), bd_idx_a)).*unit;
+            v = w./norm(w);
+            Q2 = eye(d) - 2*v(1:d)*transpose(v(1:d));
+            
+            % Compute new bottom/right neighbors of block-diagonal of A
+            A(bd_idx_c:bd_idx_d, bd_idx_a:bd_idx_b) = transpose(Q2) * A(bd_idx_c:bd_idx_d, bd_idx_a:bd_idx_b);
+            A(bd_idx_a:bd_idx_b, bd_idx_c:bd_idx_d) = transpose(A(bd_idx_c:bd_idx_d, bd_idx_a:bd_idx_b));
+            
+            % Copy over new Q1
+            Q1 = Q2;
+        end
     end
 end
-
-% Compute Q2
-% Q2 = eye(dim-1) - 2*transpose(v)*v;
-Q2 = eye(d+1) - 2*transpose(v)*v;
-
-% Compute the above block-diagonal's bottom-right neighbors
-A(3:n-0, 2) = Q2(2:n-1, 2:n-1)*transpose(A(3:n-0, 2))*Q1(2:n-1, 2:n-1)
 
 A
 
 toc
-% clear Q alpha b dim i k r v;
+clear unit Q1 Q2 alpha b dim i k r v bd_idx_a bd_idx_b bd_idx_c bd_idx_d beta d n rad u w ans;
--- matlab/BrunoLang_2_1_Find_b_r.m
+++ matlab/BrunoLang_2_1_Find_b_r.m
@@ -1,5 +1,5 @@
-function [b, r] = BrunoLang_2_1_Find_b_r(n, d, v)
-%   [b, r] = BrunoLang_2_1_Find_b_r(n, d, v) - Selects the appropriate
+function [b, r] = BrunoLang_2_1_Find_b_r(n, d, u)
+%   [b, r] = BrunoLang_2_1_Find_b_r(n, d, u) - Selects the appropriate
 %   iteration parameters for Bruno Lang's symmetric banded matrix
 %   tridiagonal reduction 2.1 algorithm.  Returns [0, 0] if there exists
 %   no sufficient values for b and r, which indicates the algorithm is
@@ -7,17 +7,17 @@ function [b, r] = BrunoLang_2_1_Find_b_r(n, d, v)
 %
 %   n - Domain elements
 %   d - Semi-bandwidth
-%   v - Current reduction step
+%   u - Current reduction step
 
 % Educated guess for parameters
-b = floor((n - v + 1)/2);
-r = n - v - d*(b - 1);
+b = floor((n - u + 1)/2);
+r = n - u - d*(b - 1);
 
 % Confirm validity of computed parameters & optimize if possible
 if(r > b)
     while((r > b) && (r > 0) && (b > 0))
         b = b - 1;
-        r = n - v - d*(b - 1);
+        r = n - u - d*(b - 1);
     end
     
     if((r > b) || (r < 1) || (b < 1))
@@ -25,13 +25,13 @@ if(r > b)
        r = 0;
     end
 elseif(r < 1)
-    b = floor((n - v - 1)/d) + 1;
-    r = n - v - d*(b - 1);
+    b = floor((n - u - 1)/d) + 1;
+    r = n - u - d*(b - 1);
     
     if(r > b)
         while((r > b) && (r > 0) && (b > 0))
             b = b - 1;
-            r = n - v - d*(b - 1);
+            r = n - u - d*(b - 1);
         end
 
         if((r > b) || (r < 1) || (b < 1))
